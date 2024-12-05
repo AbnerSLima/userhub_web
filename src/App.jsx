c8b3bdcced6f851371b7ffacd423968bc245023c
AbnerSLima <abnerdslima@gmail.com>
2024-12-05 17:40:18 -0300

    create_display

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import userHubLogo1 from '/logo1.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Aqui você pode adicionar a lógica para autenticação
+    alert(`Login\nEmail: ${email}\nSenha: ${password}`);
+    navigate("/home");
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a target="_blank">
+          <img src={userHubLogo1} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="title">Bem-vindo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input
+          type='email'
+          placeholder="Digite seu e-mail..."
+          className="input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Digite sua senha..."
+          className="input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="button" onClick={handleLogin}>
+          Entrar
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <a>
+          <p className="link">Esqueci minha senha</p>
+        </a>
+        <a>
+          <p className="link">Ainda não tem conta? Cadastre-se!</p>
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
