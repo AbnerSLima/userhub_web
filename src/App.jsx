ec9e1842473677d96df7a704f303188265a90c7e
AbnerSLima <abnerdslima@gmail.com>
2024-12-07 14:29:41 -0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import userHubLogo1 from '/logo1.png'
-import { useNavigate, Link } from "react-router-dom";
+import { BrowserRouter as Route, useNavigate, Link, Router, Routes } from "react-router-dom";
+import Home from "./authPages/home";
+import Login from "./publicPages/login";
+import Register from "./publicPages/register";
 import './App.css'
 
 function App() {
@@ -10,44 +13,42 @@ function App() {
   const [password, setPassword] = useState("");
 
   const handleLogin = () => {
-    alert(`Login\nEmail: ${email}\nSenha: ${password}`);
     navigate("/home");
   };
 
   return (
     <>
-      <div>
-        <Link target="_blank">
-          <img src={userHubLogo1} className="logo" alt="UserHub logo" />
-        </Link>
-      </div>
-      <h1 className="title">Bem-vindo</h1>
-      <div className="card">
-        <input
-          type='email'
-          placeholder="Digite seu e-mail..."
-          className="input"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Digite sua senha..."
-          className="input"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="button" onClick={handleLogin}>
-          Entrar
-        </button>
-        <a>
-          <p className="link">Esqueci minha senha</p>
-        </a>
-        <a>
-          <p className="link">Ainda não tem conta? Cadastre-se!</p>
-        </a>
-      </div>
-      
+        <div>
+          <Link target="_blank">
+            <img src={userHubLogo1} className="logo" alt="UserHub logo" />
+          </Link>
+        </div>
+        <h1 className="title">Bem-vindo</h1>
+        <div className="card">
+          <input
+            type='email'
+            placeholder="Digite seu e-mail..."
+            className="input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+          <input
+            type="password"
+            placeholder="Digite sua senha..."
+            className="input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+          <button className="button" onClick={handleLogin}>
+            Entrar
+          </button>
+          <a>
+            <p className="link">Esqueci minha senha</p>
+          </a>
+          <a>
+            <p className="link">Ainda não tem conta? Cadastre-se!</p>
+          </a>
+        </div>
     </>
   )
 }
