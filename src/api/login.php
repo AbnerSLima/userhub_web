4fa0c42bf71b57c49ed086a844dfa5c1876489cc
AbnerSLima <abnerdslima@gmail.com>
2024-12-15 20:20:12 -0300

    update_login

--- src/api/login.php
+++ src/api/login.php
@@ -13,8 +13,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 require 'conexao.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
-var_dump($data);
-
 
 $login = $data['login'] ?? null;
 $senha = $data['senha'] ?? null;
